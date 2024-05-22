a27020bc702367e9b02e6058bd3dc6bab3274bd0
Joe Mon <aruljoe37@gmail.com>
2024-05-22 23:12:55 +0530

    Lyric player with text is done

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,112 @@ class Lyric {
   }
 }
 
+class HtmlBasedLyricUI {
+  textBasedCanvasArea;
+  progressBar;
+  subtractButton;
+  addButton;
+  fontNameSelector;
+  textColorPicker1;
+  textColorPicker2;
+  bgColorPicker1;
+  bgColorPicker2;
+
+  constructor() {
+    this.textBasedCanvasArea = <HTMLDivElement>document.querySelector("#textBasedLyric .textBasedCanvasArea");
+    this.progressBar = <HTMLProgressElement>document.querySelector("#textBasedLyric .progress-bar-values");
+    this.subtractButton = <HTMLElement>this.progressBar?.nextElementSibling?.nextElementSibling;
+    this.addButton = <HTMLElement>this.subtractButton.nextElementSibling;
+    this.fontNameSelector = <HTMLSelectElement>document.querySelector("#textBasedLyric .fontName");
+    this.textColorPicker1 = <HTMLInputElement>document.querySelector("#textBasedLyric .textColorPicker1");
+    this.textColorPicker2 = <HTMLInputElement>document.querySelector("#textBasedLyric .textColorPicker2");
+    this.bgColorPicker1 = <HTMLInputElement>document.querySelector("#textBasedLyric .bgColorPicker1");
+    this.bgColorPicker2 = <HTMLInputElement>document.querySelector("#textBasedLyric .bgColorPicker2");
+    this.textBasedCanvasArea.style.width = "60%";
+  }
+
+  init() {
+    this.addButton.addEventListener("click", () => {
+      const value = Math.min(100, this.progressBar?.value + 10);
+      this.progressBar.value = value;
+      this.progressBar.textContent = `${value}%`;
+      this.textBasedCanvasArea.style.width = `${value}%`;
+    });
+
+    this.subtractButton.addEventListener("click", () => {
+      const value = Math.max(0, this.progressBar.value - 10);
+      this.progressBar.value = value;
+      this.progressBar.textContent = `${value}%`;
+      this.textBasedCanvasArea.style.width = `${value}%`;
+    });
+
+    this.fontNameSelector.addEventListener("sl-change", () => {
+      let index = this.fontNameSelector.value;
+      this.loadFont(Number(index));
+    });
+
+    this.textColorPicker1.addEventListener("sl-input", (e) => {
+      document.documentElement.style.setProperty("--textColor1", (e.target as HTMLInputElement)?.value);
+    });
+
+    this.textColorPicker2.addEventListener("sl-input", (e) => {
+      document.documentElement.style.setProperty("--textColor2", (e.target as HTMLInputElement)?.value);
+    });
+
+    this.bgColorPicker1.addEventListener("sl-input", (e) => {
+      console.log(e);
+      document.documentElement.style.setProperty("--bgColor1", (e.target as HTMLInputElement)?.value);
+    });
+
+    this.bgColorPicker2.addEventListener("sl-input", (e) => {
+      document.documentElement.style.setProperty("--bgColor2", (e.target as HTMLInputElement)?.value);
+    });
+  }
+
+  loadFont(index: number) {
+    const fonts = [
+      {
+        name: "Shadows Into Light",
+        url: "https://fonts.gstatic.com/s/shadowsintolight/v19/UqyNK9UOIntux_czAvDQx_ZcHqZXBNQzdcD5.woff2",
+      },
+      { name: "Indie Flower", url: "https://fonts.gstatic.com/s/indieflower/v21/m8JVjfNVeKWVnh3QMuKkFcZVaUuH.woff2" },
+      { name: "Monoton", url: "https://fonts.gstatic.com/s/monoton/v19/5h1aiZUrOngCibe4TkHLQg.woff2" },
+      {
+        name: "Mystery Quest",
+        url: "https://fonts.gstatic.com/s/mysteryquest/v20/-nF6OG414u0E6k0wynSGlujRLwYvDg.woff2",
+      },
+      {
+        name: "Bungee Hairline",
+        url: "https://fonts.gstatic.com/s/bungeehairline/v22/snfys0G548t04270a_ljTLUVrv-LZxec.woff2",
+      },
+    ];
+
+    let choosenFont = fonts[index];
+
+    var myfont = new FontFace(choosenFont.name, `url(${choosenFont.url}) format("woff2")`);
+    myfont
+      .load()
+      .then(function (loadedFont) {
+        document.fonts.add(loadedFont);
+        let lyricText = <HTMLHeadingElement>document.querySelector("#textBasedLyric h2");
+        lyricText.style.fontFamily = choosenFont.name;
+        if (choosenFont.name === "Bungee Hairline") {
+          lyricText.style.fontWeight = "bold";
+          lyricText.style.lineHeight = "62px";
+        } else {
+          lyricText.style.fontWeight = "normal";
+          lyricText.style.lineHeight = "initial";
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
+
+let htmlbased = new HtmlBasedLyricUI();
+htmlbased.init();
+
 class UtilityFunctions {
   static convertFromSecondsToMs(seconds: number) {
     return seconds * 1000;
